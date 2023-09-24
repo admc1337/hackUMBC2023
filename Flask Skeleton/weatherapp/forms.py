37e86f76d96db1c6be97aab4769814e414a64ea9
Joel D <88012270+joelDev28@users.noreply.github.com>
2023-09-24 11:35:45 -0400

    Add files via upload

---
+++ Flask Skeleton/weatherapp/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+class myForm(FlaskForm):
+    zipcode = StringField('ZipCode', 
+                         validators=[Length(min=5,max=8)])
+    submit = SubmitField('Search Weather')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', 
+                           validators=[Length(min=2,max=20)])
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', 
+                             validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', 
+                             validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', 
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', 
+                             validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+    
